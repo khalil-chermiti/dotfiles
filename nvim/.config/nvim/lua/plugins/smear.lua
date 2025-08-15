0bb41e250b3c53427029debba037adc7de2376a4
khalil chermiti <khalil.chermiti@proton.me>
2025-08-15 22:27:29 +0100

    feat(ui): add cursor animation

---
+++ nvim/.config/nvim/lua/plugins/smear.lua
@@ -0,0 +1,9 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    legacy_computing_symbols_support = true,
+    transparent_bg_fallback_color = "#303030",
+    hide_target_hack = true,
+    never_draw_over_target = true,
+  },
+}
--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -6,12 +6,12 @@ return {
     input = { enabled = true },
     notifier = { enabled = true },
     scope = { enabled = true },
-    scroll = { enabled = true },
+    scroll = { enabled = false},
     statuscolumn = { enabled = false }, -- we set this in options.lua
     toggle = { map = LazyVim.safe_keymap_set },
     words = { enabled = true },
     dashboard = { enabled = false },
-    explorer = { enabled = false},
+    explorer = { enabled = false },
 
     picker = {
       sources = {
